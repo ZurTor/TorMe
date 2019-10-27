ba4dd57bf42d8f0d326dd0b7a80dcb980ccfc14c
Krabbens <k.gasiorowski@g.loiv.torun.pl>
2019-10-27 13:02:29 -0700

    Niedziela 2

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ host = '127.0.0.1'
 port = 1337
 socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
 soc = socks.socksocket()
-soc.connect((qnvzyabvnrx5twf7rpbbhklmlgvn2rdq3hkvpgnq5rik3gnvalybl6ad.onion, port))
-login = map(str, (input().split(' ')))
-login_bytes = pickle.dumps(arr)
-soc.sendall(login_bytes)
+soc.connect(("qnvzyabvnrx5twf7rpbbhklmlgvn2rdq3hkvpgnq5rik3gnvalybl6ad.onion", port))
+while True:
+    login = list(map(str,input().split()))
+    login_bytes = pickle.dumps(login)
+    soc.sendall(login_bytes)
 
 print('Received', soc.recv(4096))
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import mysql.connector
 import socket
 import socks
 import pickle
+from threading import Thread
 login = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -10,20 +11,24 @@ login = mysql.connector.connect(
 host = '127.0.0.1'
 port = 1337
 socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
+
+def on_new_client(clientsocket,addr):
+    print('Connected by: ', addr)
+    while True:
+        data = conn.recv(4096)
+        if not data: break
+        data_map = pickle.loads(data)
+        print(repr(data_map))
+        clientsocket.send(b"Delivered")
+    clientsocket.close()
+
+
+
 soc = socks.socksocket()
 soc.bind((host, port))
 soc.listen()
-conn, addr = soc.accept()
-print(login)
-pointer = login.cursor()
-with conn:
-        print('Connected by: ', addr)
-        while True:
-            data = conn.recv(4096)
-            data_map = pickle.loads(data)
-            print(repr(data_map))
-            if not data: break
-
-            #conn.send(data)
-            #sql = 'INSERT INTO login (username, password) VALUES (%s, %s)'
-            #val = (data,)
+#print(login)
+#pointer = login.cursor()
+while True:
+    conn, addr = soc.accept()
+    Thread(target=on_new_client,args=(conn,addr)).
