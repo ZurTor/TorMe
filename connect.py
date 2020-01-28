104dfffb05a3315f237a039a94b905cc6c924501
ZurTor <56635811+ZurTor@users.noreply.github.com>
2020-01-28 22:58:17 +0100

    +1

--- connect.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-
-def run_tor():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.system("killall tor")
-    return subprocess.Popen([dir_path + '/tor'])
--- electron/resources/app/server.py
+++ electron/resources/app/server.py
@@ -17,29 +17,33 @@ database = mysql.connector.connect(
   database="login"
 )
 cursor = database.cursor()
+def is_json(myjson):
+    try:
+        json_object = json.loads(myjson)
+    except:
+        return False
+    return True
 sqlReg = "INSERT INTO userdata (username, password, pubkey) VALUES (%s, %s, %s)"
 host = '127.0.0.1'
 port = 1337
 socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
-def checkMsg():
-    pass
-def register(jsonReg, pubkey, conn):
+def register(jsonReg, pubkey, conn, cursor):
     print("\nUSERNAME: ", jsonReg["username"])
     cursor.execute("SELECT * from userdata WHERE username = %(username)s", {'username': jsonReg["username"]})
-    rows = cursor.fetchone()
+    rows = list(cursor.fetchone())
     if rows == None:
         cursor.execute(sqlReg, (jsonReg["username"], hashSHA(jsonReg["password"]), pubkey["pubkey"]))
         database.commit()
     else:
         conn.sendall(pickle.dumps("accex"))
 
-def sendKey(jsonLog, conn):
+def sendKey(jsonLog, conn, cursor):
     cursor.execute("SELECT pubkey from userdata WHERE username = %(username)s", {'username': jsonLog["username"]})
-    rows = cursor.fetchone()
+    rows = list(cursor.fetchone())
     conn.sendall(pickle.dumps(rows[0]))
-def login(jsonLog, conn, pubkey):
+def login(jsonLog, conn, pubkey, cursor):
     cursor.execute("SELECT * from userdata WHERE username = %(username)s", {'username': jsonLog["username"]})
-    rows = cursor.fetchone()
+    rows = list(cursor.fetchone())
     if rows == None:
         conn.sendall(pickle.dumps("noacc"))
     #database.commit()
@@ -56,32 +60,35 @@ def login(jsonLog, conn, pubkey):
             conn.sendall(pickle.dumps("bpass"))
     else:
         conn.sendall(pickle.dumps("Wrong key!"))
-def addMsg(jsonLog, message):
+
+def addMsg(jsonLog, message, cursor):
     cursor.execute("SELECT token from userdata WHERE username = %(username)s", {'username': jsonLog["mainUser"]})
-    token = cursor.fetchone()
+    token = list(cursor.fetchone())
     cursor.execute("SELECT data from userdata WHERE username = %(username)s", {'username': jsonLog["destUser"]})
-    data = cursor.fetchone()
+    data = list(cursor.fetchone())
     if token[0].decode() == hashSHA(jsonLog["token"]):
         try:
             picklist = pickle.loads(data[0])
+            picklist.append(jsonLog["time"])
             picklist.append(jsonLog["mainUser"])
             picklist.append(message)
         except:
             picklist = []
+            picklist.append(jsonLog["time"])
             picklist.append(jsonLog["mainUser"])
             picklist.append(message)
         dataBack = pickle.dumps(picklist)
         print(jsonLog["destUser"])
         cursor.execute('''UPDATE userdata SET data = %s WHERE username = %s''', (dataBack, jsonLog["destUser"]))
         database.commit()
-def getMsg(jsonLog, conn):
+def getMsg(jsonLog, conn, cursor):
     print("pierwszy mysql")
     print(jsonLog["username"])
     try:
         cursor.execute('''SELECT username from userdata WHERE username = %(username)s''', {'username' : jsonLog["username"]})
     except:
         conn.close()
-    userBase = cursor.fetchone()
+    userBase = list(cursor.fetchone())
     print(userBase[0])
     if userBase[0].decode() == jsonLog["username"]:
         print("jest")
@@ -90,13 +97,12 @@ def getMsg(jsonLog, conn):
         except:
             conn.close()
         print("token")
-        data = cursor.fetchone()[0]
+        data = list(cursor.fetchone())[0]
         print(data)
         print("pobraned")
         conn.sendall(data + b'XDD')
         cursor.execute('''UPDATE userdata SET data = "" WHERE token = %(token)s''', {'token' : hashSHA(jsonLog["token"])})
         database.commit()
-    conn.close()
 
 def hashSHA(mess):
     keccak_hash = keccak.new(digest_bits=512)
@@ -116,16 +122,18 @@ def on_new_client(clientsocket,addr):
     print(jsonLog)
     if jsonLog["type"] == "login":
         pubkey = json.loads(data_map_list[1])
-        login(jsonLog, clientsocket, pubkey)
+        login(jsonLog, clientsocket, pubkey, cursor)
     if jsonLog["type"] == "register":
         pubkey = json.loads(data_map_list[1])
-        register(jsonLog, pubkey, clientsocket)
+        register(jsonLog, pubkey, clientsocket, cursor)
     if jsonLog["type"] == "getKey":
-        sendKey(jsonLog, clientsocket)
+        sendKey(jsonLog, clientsocket, cursor)
     if jsonLog["type"] == "sendMsg":
-        addMsg(jsonLog, data_map_list[1])
+        addMsg(jsonLog, data_map_list[1], cursor)
     if jsonLog["type"] == "getMsg":
-        getMsg(jsonLog, clientsocket)
+        getMsg(jsonLog, clientsocket, cursor)
+    clientsocket.close()
+    exit()
 
 soc = socks.socksocket(socket.AF_INET, socket.SOCK_STREAM)
 soc.bind((host, port))
